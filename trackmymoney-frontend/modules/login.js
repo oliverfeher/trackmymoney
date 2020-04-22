73c3d7e5a763cf3971e9dcdbdfd6cc84847ef366
oliverfeher <oliver92.feher@gmail.com>
2020-04-22 03:44:20 -0400

    Build login form container
    - Add logo
    - Add logo text
    - Add container

--- trackmymoney-frontend/modules/login.js
+++ trackmymoney-frontend/modules/login.js
@@ -1,18 +1,26 @@
-const main = document.querySelector("main");
-
-const startUp = () => 
+const logInForm = () => 
 {
-    const div = document.createElement("div");
-    div.setAttribute("id", "main-container")
-    main.append(div)
-
-    const backgroundPic = document.createElement("img");
-    backgroundPic.setAttribute("id", "login-background");
-    backgroundPic.setAttribute("src", "./assets/login-background.svg");
-    
+    const container = document.querySelector("#main-container")
     const loginContainer = document.createElement("div");
     loginContainer.setAttribute("id", "login-container")
-    div.append(backgroundPic, loginContainer);
+    
+    const title = document.createElement("div");
+    title.setAttribute("id", "title-container");
+
+    const logo = document.createElement("img");
+    logo.setAttribute("src", "./assets/logo.svg");
+    logo.setAttribute("id", "logo")
+
+    const titleText = document.createElement("p");
+    titleText.setAttribute("id", "title-text");
+    titleText.innerText = "trackMyMoney";
+
+    title.append(logo, titleText);
+
+    loginContainer.append(title)
+    container.append(loginContainer);
 }
 
-export default startUp;
+
+
+export default logInForm;
--- trackmymoney-frontend/src/index.js
+++ trackmymoney-frontend/src/index.js
@@ -1,3 +1,15 @@
-import startUp from "/modules/login.js";
+import logInForm from "/modules/login.js";
 
-document.onload = startUp();
+const main = document.querySelector("main");
+const div = document.createElement("div");
+div.setAttribute("id", "main-container")
+main.append(div)
+
+
+const backgroundPic = document.createElement("img");
+backgroundPic.setAttribute("id", "login-background");
+backgroundPic.setAttribute("src", "./assets/login-background.svg");
+div.append(backgroundPic)
+
+
+logInForm();
