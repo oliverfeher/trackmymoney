12521719d123a74d176466ae3dca5770ae46c6d3
oliverfeher <oliver92.feher@gmail.com>
2020-04-22 02:01:26 -0400

    Add first/last name to users table
    - Create test user
    - Render users json

--- trackmymoney-backend/db/schema.rb
+++ trackmymoney-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_054353) do
+ActiveRecord::Schema.define(version: 2020_04_22_055439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_04_22_054353) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
