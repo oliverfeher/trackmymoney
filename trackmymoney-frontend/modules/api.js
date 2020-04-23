12dd0085d23141356a05d20d526013f632ba8fff
oliverfeher <oliver92.feher@gmail.com>
2020-04-22 22:09:52 -0400

    Build API adapter POST
    - Build universal fetch post request
    - Use config obj for post request
    - Use baseURL

--- trackmymoney-frontend/modules/api.js
+++ trackmymoney-frontend/modules/api.js
@@ -1,4 +1,27 @@
 class ApiAdapter
 {
-    
+
+    // FETCH HEADERS CONFIG
+    static headersConfig = 
+    {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    };
+
+    // BASE URL
+    static baseURL = "https://localhost:3000";
+
+
+    // UNIVERSAL FETCH POST REQUEST
+    static postRequest(url, body)
+    {
+        return fetch(this.baseURL + url, {
+            method: "POST",
+            headers: this.headersConfig,
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json());
+    };
+
+
 }
