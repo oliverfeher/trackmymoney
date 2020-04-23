11f5fcb35bb6236028254825118c04de357460b0
oliverfeher <oliver92.feher@gmail.com>
2020-04-23 18:47:28 -0400

    Build loadMainPage()
    - Add renderLoggedInHeader()
    - Add/build renderIncomeSection

--- trackmymoney-frontend/modules/dom.js
+++ trackmymoney-frontend/modules/dom.js
@@ -32,14 +32,44 @@ class Dom
     // RERENDER AFTER SUCCESFUL LOGIN
     static loadMainPage()
     {
-        this.container.innerHTML =  `
+        
+        this.container.innerHTML = `${this.renderLoggedInHeader()} + ${this.renderIncomeSection()}`;
+    };
+
+    // RENDER LOGGED IN HEADER
+
+    static renderLoggedInHeader()
+    {
+        return `
         <div id="user-container">
             <div class="title-container" id="logged-in-title">
                 <img src="./assets/logo.svg" alt="logo" id="logo"/>
                 <p id="title-text">trackMyMoney</p>
             </div>
-            <p id="welcome-msg">Welcom, ${Auth.currentUser.first_name + " " + Auth.currentUser.last_name} !</p>
+            <div id="welcome-container">
+                <p id="welcome-msg">Welcome, ${Auth.currentUser.first_name + " " + Auth.currentUser.last_name}!</p>
+                <p id="date-now">${this.getTodaysDate()}</p>
+            </div>
         </div>`
+    }
+
+    // RENDER INCOME SECTION\
+    static renderIncomeSection() 
+    {
+        return `
+            <div id="income-section">
+                
+            </div>
+        `
+    }
+
+    // RENDER CURRENT DATE
+    static getTodaysDate()
+    {
+        let date = new Date();
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let currentDate = ` ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+        return currentDate;
     }
 }
 
