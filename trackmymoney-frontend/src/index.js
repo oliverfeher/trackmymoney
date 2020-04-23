ad5a7fd1bfecb965cc08984e6b98207e70424f26
oliverfeher <oliver92.feher@gmail.com>
2020-04-22 21:42:30 -0400

    Refactor index html

--- trackmymoney-frontend/src/index.js
+++ trackmymoney-frontend/src/index.js
@@ -1,10 +1,5 @@
 import Auth from "/modules/login.js";
 
-const main = document.querySelector("main");
-const div = document.createElement("div");
-div.setAttribute("id", "main-container")
-main.append(div)
-
 
 Auth.logInForm();
 
